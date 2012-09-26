003974e58753c56fc558720283029111b787118f
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-09-26 13:37:54 +0200

    3rd article

--- src/numbers_test_03.erl
+++ src/numbers_test_03.erl
@@ -2,5 +2,9 @@
 -include_lib("eunit/include/eunit.hrl").
 
 simple_test() ->
-    ?_assert(1 == 1).
+    ?assert(1 == 1),
+    ?assertNot(1 == 0),
+    ?assertMatch({number, _Var}, {number, 42}),
+    ?assertEqual(1, 1),
+    ?assertError(undef, lists:append()).
     
